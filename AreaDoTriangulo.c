6fee04e41a1a93857e2bd96a33de9c2db2207569
pedro.bat <160679378+PedroDaniel-dev@users.noreply.github.com>
2025-04-11 12:59:28 -0300

    Create AreaDoTriangulo.c

---
+++ AreaDoTriangulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int base, altura;
+    printf("Digite a base do triangulo:");
+    scanf("%d", &base);
+    printf("Digite a altura do triangulo:");
+    scanf("%d:", &altura);
+    printf("A area do triangulo é: %d", (base * altura) / 2);
+}
