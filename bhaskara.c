7672e6bc6f7ad1edf60d482bddc6db215774f6f4
pedro.bat <160679378+PedroDaniel-dev@users.noreply.github.com>
2025-04-16 15:42:36 -0300

    Create bhaskara.c

---
+++ bhaskara.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, x, c, delta; 
+    float raiz1, raiz2;
+    
+    printf("Digite a b c.\n");
+    scanf("%d, %d, %d", &a, &b, &c);
+    
+    delta = (float)(b * b) - 4 * a * c;
+    printf("%d", delta);
+    raiz1 = -b + sqrt(delta) / (2 * a);
+    raiz2 = -b - sqrt(delta) / (2 * a);
+
+    if(delta == 0 && delta < 0) {
+        printf("A equaçao nao possui raiz real.");
+    }
+    else if (a == 0) {
+        printf("Sua funcao nao eh do segundo grau.\n");
+    }
+    else {
+        printf("Raiz': %.f\nRaiz'': %.f\n", raiz1, raiz2);
+    }
+    
+}
