54a1dd6f6b420282e3219a6d3142a534fe9d9f1a
pedro.bat <160679378+PedroDaniel-dev@users.noreply.github.com>
2025-04-16 13:09:25 -0300

    Create funcao.c

---
+++ funcao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float a , b, x;
+    
+    printf("Digite o valor de a.\n");
+    scanf("%f", &a);
+    
+    printf("Digite o valor de b.\n");
+    scanf("%f", &b);
+    
+    if (a == 0 ) {
+        printf("Sua funcao função eh nula.\n");
+            
+    }
+    else {
+            printf("%.1f", -b / a);
+
+    }
