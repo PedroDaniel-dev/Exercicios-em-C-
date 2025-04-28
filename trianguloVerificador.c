05e86b28e75b7af431c109c60c18c81e5231a8c7
pedro.bat <160679378+PedroDaniel-dev@users.noreply.github.com>
2025-04-28 12:33:01 -0300

    Update trianguloVerificador.c

--- trianguloVerificador.c
+++ trianguloVerificador.c
@@ -2,16 +2,23 @@
 #include <math.h>
 
 int main() {
-    int cateto1, cateto2, hipotenusa, triangulo;
-    printf("Digite os dois catetos e a hipotenusa.");
-    scanf("%d %d %d", &cateto1, &cateto2, &hipotenusa);
-    
-    if (pow(cateto1, 2) + pow(cateto2, 2) == pow(hipotenusa, 2)) {
-        printf("Triangulo real.");
+    int lado1, lado2, lado3;
+    printf("Digite os tres lados do triangulo.");
+    scanf("%d %d %d", &lado1, &lado2, &lado3);
+
+    if (lado1 + lado2 > lado3 && lado2 + lado3 > lado1 && lado1 + lado3 > lado2) {
+        printf("Triangulo real ");
+                if (lado1 == lado2 && lado2 == lado3) {
+                    printf("equilatero");
+                }else if ((lado1 == lado2 && lado1 != lado3) ||
+                        (lado1 == lado3 && lado1 != lado2) ||
+                        (lado2 == lado3 && lado2 !=lado1)) {
+                        printf("isoceles");
+                }else {
+                    printf("escaleno");
+                    }
     }
-    
     else {
         printf("Nao Triangulo.");
     }
-    
 }
